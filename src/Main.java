a6629af138d17c4241167f12d5ba07611bf597b4
Youssif El-Sayed <Yousefd4704@gmail.com>
2024-11-17 21:37:38 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import  java.util.Scanner;
+import static java.lang.System.out;
+import java.io.*;
+
+public class Main {
+    public static  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
